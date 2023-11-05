c6a6b0ee1fce0e62b40211841f4b0e1d3ecec5f4
Lucky Ivanius <luckzivanius@gmail.com>
2023-11-06 01:01:05 +0700

    Add users repository

---
+++ src/core/repositories/users.repository.ts
@@ -0,0 +1,6 @@
+import { UniqueId } from '../common/domain/unique-id';
+import { User } from '../entities/user/user';
+
+export interface UsersRepository {
+  getUserById(userId: UniqueId): Promise<User>;
+}
