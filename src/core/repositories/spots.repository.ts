bcdb60a512fb51fea4eaee3e753da1a254c3564e
Lucky Ivanius <luckzivanius@gmail.com>
2023-11-06 15:20:26 +0700

    Fix spots repository name

--- src/core/repositories/spots.repository.ts
+++ src/core/repositories/spots.repository.ts
@@ -1,7 +1,7 @@
 import { UniqueId } from '../common/domain/unique-id';
 import { Spot } from '../entities/spot/spot';
 
-export interface SpotRepository {
+export interface SpotsRepository {
   getSpotById(spotId: UniqueId): Promise<Spot>;
   getSpots(): Promise<Spot[]>;
   getCyclingAccessibilitySpots(): Promise<Spot[]>;
