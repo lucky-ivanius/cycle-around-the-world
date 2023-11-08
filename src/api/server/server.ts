fa24735e9a04d99c2268bc2e88b21c9ba7989610
Lucky Ivanius <luckzivanius@gmail.com>
2023-11-08 23:40:58 +0700

    Create api express server

---
+++ src/api/server/server.ts
@@ -0,0 +1,46 @@
+import bodyParser from 'body-parser';
+import compression from 'compression';
+import cors, { CorsOptions } from 'cors';
+import express from 'express';
+import { queryParser } from 'express-query-parser';
+import helmet from 'helmet';
+import morgan from 'morgan';
+
+import { apiConfig } from '../config/api.config';
+
+import { Controller } from '../../infrastructure/http/common/controller';
+import { v1Router } from '../routes/api.router';
+
+const corsOptions: CorsOptions = {
+  origin: apiConfig.origin,
+};
+
+const server = express();
+
+server
+  .use(bodyParser.json())
+  .use(bodyParser.urlencoded({ extended: true }))
+  .use(
+    queryParser({
+      parseNumber: true,
+      parseBoolean: true,
+      parseNull: true,
+      parseUndefined: true,
+    })
+  )
+  .use(cors(corsOptions))
+  .use(compression())
+  .use(helmet())
+  .use(morgan('dev'));
+
+server.get('/', (req, res) => Controller.ok(res, { message: 'Hi 💕' }));
+server.use('/api/v1', v1Router);
+server.use('*', (req, res) => Controller.notFound(res));
+
+const port = apiConfig.port;
+
+server.listen(port, () => {
+  console.log(`Running on port ${port}! 🚀🚀🚀`);
+});
+
+export { server };
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,1 @@
-console.log('Hi');
+import './api/server/server';
