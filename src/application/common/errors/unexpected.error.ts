5ff29ef1ae340b7aebbb66c2897de33e45a859b3
Lucky Ivanius <luckzivanius@gmail.com>
2023-11-09 02:33:48 +0700

    add error log for unexpected error

--- src/application/common/errors/unexpected.error.ts
+++ src/application/common/errors/unexpected.error.ts
@@ -2,6 +2,7 @@ import { Result } from '../../../core/common/logic/result';
 
 export class UnexpectedError extends Result<unknown> {
   constructor(err: unknown) {
+    console.error(err);
     super(false, err, 'An unexpected error occured');
   }
 }
