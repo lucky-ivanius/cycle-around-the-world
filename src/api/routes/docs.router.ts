7a18801967acaeeaa77e3ee4b006bd4fd368fa83
Lucky Ivanius <luckzivanius@gmail.com>
2023-11-10 20:28:07 +0700

    Fix swagger api css url

--- src/api/routes/docs.router.ts
+++ src/api/routes/docs.router.ts
@@ -2,9 +2,11 @@ import { Router } from 'express';
 import swaggerUi from 'swagger-ui-express';
 import docs from '../docs/index.json';
 
+const customCssUrl =
+  'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.1.0/swagger-ui.min.css';
+
 const docsRouter = Router();
 
-docsRouter.use('/', swaggerUi.serve);
-docsRouter.use('/', swaggerUi.setup(docs));
+docsRouter.use('/', swaggerUi.serve, swaggerUi.setup(docs, { customCssUrl }));
 
 export { docsRouter };
